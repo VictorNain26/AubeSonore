a7119e825929917800287a6d9de996a1cb6816d3
Aube Sonore <contact@aubesonore.com>
2026-01-18 14:45:42 +0100

    fix(mobile): resolve babel plugin conflict for reanimated/worklets
    Add unique identifiers to prevent duplicate plugin error

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -6,8 +6,8 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
-      "react-native-worklets/plugin",
-      "react-native-reanimated/plugin",
+      ["react-native-worklets/plugin", {}, "react-native-worklets"],
+      ["react-native-reanimated/plugin", {}, "react-native-reanimated"],
     ],
   };
 };
